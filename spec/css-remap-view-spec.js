6e96e31d9e73494a0c22277424a50aaa69cf4a71
Dan Fessler <dan@manticoregames.com>
2019-01-07 18:40:43 -0800

    Initial commit

---
+++ spec/css-remap-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CssRemapView from '../lib/css-remap-view';
+
+describe('CssRemapView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
