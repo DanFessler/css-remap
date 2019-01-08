0b27b28e0cb0538ccefef4a2c99b4855d595f91f
Dan Fessler <fezzinate@gmail.com>
2019-01-07 23:14:58 -0800

    .

--- spec/css-remap-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CssRemapView from '../lib/css-remap-view';
-
-describe('CssRemapView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
